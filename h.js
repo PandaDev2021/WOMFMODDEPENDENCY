e30ee1e1231d465d86afd7524804a989a0c91778
P.Dev <85044888+PandaDev2021@users.noreply.github.com>
2022-01-15 18:26:10 -0500

    Update h.js

--- h.js
+++ h.js
@@ -1,11 +1,2 @@
-var htmlfile = WSH.CreateObject('htmlfile'), JSON;
-htmlfile.write('<meta http-equiv="x-ua-compatible" content="IE=9" />');
-htmlfile.close(JSON = htmlfile.parentWindow.JSON);
-function includeFile(filename)
-{
-  var fso = new ActiveXObject("Scripting.FileSystemObject");
-  var fileStream = fso.openTextFile(filename);
-  var fileData = fileStream.readAll();
-  fileStream.Close();
-  return fileData;
-}
+var data = fs.readFileSync("C:/minecraftAutoUpdate/m.json", {encoding:'utf-8',flag:'r'})
+console.log(JSON.parse(data));
