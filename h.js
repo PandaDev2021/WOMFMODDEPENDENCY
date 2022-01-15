8c13fe5e816501c76ee9445d2244ffd1cb67b000
PandaDev2021 <85044888+PandaDev2021@users.noreply.github.com>
2022-01-15 18:40:27 -0500

    updated h

--- h.js
+++ h.js
@@ -1,2 +1,6 @@
-var data = fs.readFileSync("C:/minecraftAutoUpdate/m.json", {encoding:'utf-8',flag:'r'})
-console.log(JSON.parse(data));
+const fs = require('fs');
+const data = fs.readFileSync("C:/minecraftAutoUpdate/m.json", {encoding:'utf-8',flag:'r'})
+const iterableD = JSON.parse(data);
+iterableD.forEach(modFile => {
+    console.log(modFile.name,modFile.download_url)
+})
