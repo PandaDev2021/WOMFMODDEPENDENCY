0f23015a614f5377e05b75c865878d9d19839c39
PandaDev2021 <85044888+PandaDev2021@users.noreply.github.com>
2022-01-15 18:56:32 -0500

    updated JS

--- h.js
+++ h.js
@@ -2,16 +2,19 @@ const fs = require('fs');
 const { exec } = require("child_process");
 const data = fs.readFileSync("C:/minecraftAutoUpdate/m.json", {encoding:'utf-8',flag:'r'})
 const iterableD = JSON.parse(data);
+console.log(iterableD)
 let dls = [];
-iterableD.forEach(modFile => {
+iterableD.forEach((modFile) => {
     console.log(modFile.name,modFile.download_url)
-    if (fs.existsSync(modFile.name)) {
-        return;
-    } else {
-        dls.push(modFile);
+    if(modfile.name.includes('.jar')){
+        if (fs.existsSync(modFile.name)) {
+            return;
+        } else {
+            dls.push(modFile);
+        }
     }
 })
-dls = dls.map(x => `curl -# -o ${x.name} ${x.download_url}`)
+dls = dls.map(x => `CD %USERPROFILE%\AppData\Roaming\.minecraft && curl -# -o ${x.name} ${x.download_url}`)
 dls.forEach(cmd => {
     exec(cmd, (err, stdout, stderr) => {
         if (err) console.error(err);
